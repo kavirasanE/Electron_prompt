eb4eb89b0326104b8272fdaeb23876226fccfb44
kavirasanE <einsteinkavi98@gmail.com>
2024-05-19 20:09:35 +0530

    changes

--- app/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const App = () => {
-  return (
-    <div>Apsd,vjbsdfjdbkj,fzdvnzc,vjn zc,mj vnzc,jm nzc, nzc,. nznvds,nvbdkjvbdkjvbdkjbcjp</div>
-  )
-}
-
-export default App
